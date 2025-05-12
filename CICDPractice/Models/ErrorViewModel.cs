c3296406f43ad904a41f33853f6825254a5e6077
csant26 <shishantstha@gmail.com>
2025-05-12 12:00:10 +0545

    initial

---
+++ CICDPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICDPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
